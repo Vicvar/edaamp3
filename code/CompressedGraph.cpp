05281621f0f90414a0aafce7e97a362121c1a52d
plazasoto <jarrettsguitarsfactory@gmail.com>
2018-12-03 18:14:52 -0300

    b

--- code/CompressedGraph.cpp
+++ code/CompressedGraph.cpp
@@ -10,12 +10,30 @@ using namespace sdsl;
 CompressedGraph::CompressedGraph(vector<vector<int> > adjList){
 	//Constructs Compressed Graph from adjacency list
 	vector<int> catAL;
+	
+	int bsize = adjList.size();//tamaño de b
 
 	for(int i=0; i<adjList.size(); i++){
+
+		bsize += adjList[i].size();
+
 		for(int j=0; j<adjList[i].size(); j++){
 			catAL.push_back(adjList[i][j]);
 		}
 	}
+
+	b = bit_vector(bsize,0);
+	b[0] = 1;
+	int k = 0;
+	for(int i=0; i<adjList.size()-1; i++){
+		k += adjList[i].size() + 1;
+		b[k] = 1;
+	}
+/*for (int i = 0; i < b.size(); ++i)
+{
+	cout << b[i];
+}	cout << endl;*/
+
 
 	int * arrayAL= catAL.data();
 
